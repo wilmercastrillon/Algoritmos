5c0735bc80e8d8b9241846b9fc04ba11bff4b143
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-07-05 13:53:06 -0500

    uva 10911

--- programacion dinamica/mascara de bits.cpp
+++ programacion dinamica/mascara de bits.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 #include <string.h>
-#define forn(a, b) for(int a = 0; a < n; a++)
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
-double dp[1 << 18], p[18][18];
+double dist[20][20], dp[1 << 16], aux;
 int n;
 
-double f(int mask){
-    //cout << "mask = " << mask << endl;
-    //cout << "dp = " << dp[mask] << endl;
+double matching(int mask){
     if(dp[mask] > -0.5) return dp[mask];
-    int vivos = 0;
+    if(mask == aux) return dp[mask] = 0;
 
-    forn(i, n) if((mask >> i) % 2 == 1) vivos++;
-    double pares = (vivos*(vivos -1))/2;
-    if(vivos == 1){
-        if(mask == 1) dp[mask] = 1.;
-        else dp[mask] = 0.;
-        return dp[mask];
-    }
+    double ans = 1000000000;
+    int p1, p2;
 
-    dp[mask] = 0.;
-    forn(i, n) forn(j, i)
-        if((mask >> i)%2 == 1 && (mask >> j) %2 == 1){
-                if(i != 0 && j != 0)
-                    dp[mask] += (f(mask^(1<<i))*p[j][i]+f(mask^(1<<j))*p[i][j])/pares;
-                else if(i == 0) dp[mask] += f(mask^(1<<j))*p[i][j]/pares;
-                else if(j == 0) dp[mask] += f(mask^(1<<i))*p[j][i]/pares;
+    for(p1 = 0; p1 < 2*n; p1++)
+        if(!(mask & (1 << p1))) break;
+    for(p2 = p1 + 1; p2 < 2*n; p2++)
+        if(!(mask & (1 << p2))){
+            ans = min(ans, dist[p1][p2] + matching(mask | (1 << p1) | (1 << p2)));
         }
-    return dp[mask];
+
+    return dp[mask] = ans;
 }
 
 int main(){
-    memset(dp, -1, sizeof(dp));
+    int posx[20], posy[20], caso = 1;
 
-    cin >> n;
-    forn(i, n) forn(j, n) cin >> p[i][j];
+    while(scanf("%d", &n), n){
+        for(int i = 0; i < n*2; i++) scanf("%*s %d %d", &posx[i], &posy[i]);
 
-    printf("%.6lf\n", f((1 << n) - 1));
+        for(int i = 0; i < n*2; i++)
+            for(int j = i + 1; j < n*2; j++)
+                dist[i][j] = dist[j][i] = hypot(posx[i] - posx[j], posy[i] - posy[j]);
+
+        for(int i = 0; i < (1<<16); i++) dp[i] = -1;
+        aux = (1 << (n << 1)) - 1;
+        printf("Case %d: %.2lf\n", caso++, matching(0));
+    }
     return 0;
 }
