3b11f81caa023630e1ecd712e93b1ee64a7d9e77
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-07-10 12:47:37 -0500

    Create Max Range Sum.cpp

---
+++ programacion dinamica/Max Range Sum.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define max(a, b) (a > b)? a: b
+
+using namespace std;
+
+int main(){
+    int n, num, res, aux;
+
+    while(scanf("%d", &n), n){
+        res = aux = 0;
+        for(int i = 0; i < n; i++){
+            scanf("%d", &num);
+            aux += num;
+            res = max(aux, res);
+            if(aux < 0) aux = 0;
+        }
+
+        if(res > 0) printf("MRS = %d\n", res);
+        else printf("negativo.\n");
+    }
+
+    return 0;
+}
