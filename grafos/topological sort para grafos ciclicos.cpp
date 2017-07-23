d565a5c9bb389bd158834e2721102dd880292fce
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-07-23 18:06:30 -0500

    topogical sort

--- grafos/topological sort para grafos ciclicos.cpp
+++ grafos/topological sort para grafos ciclicos.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef queue<int> qi;
-typedef vector<qi> vqi;
+typedef vector<int> vi;
+typedef vector <vi> vii;
 
-int ent[1000010];
-vector<int> res;
+vector<int> res, ent;
 
-bool topological_sort(vqi &lis, int tam, int aris){
+void topological_sort(vii &lis, int tam){
     res.clear();
-    qi s;
+    queue<int> s;
     for(int i = 1; i <= tam; i++){
         //printf("nodo %d , tiene %d aristas entantes\n", i, ent[i]);
         if(!ent[i]) s.push(i);
@@ -21,40 +20,32 @@ bool topological_sort(vqi &lis, int tam, int aris){
         s.pop();
         res.push_back(n);
 
-        while(lis[n].size()){
-            m = lis[n].front();
-            //cout << "entra " << n << " va a " << m;
-            lis[n].pop();
+        for(int i = 0; i < lis[n].size(); i++){
+            m = lis[n][i];
             ent[m]--;
-            aris--;
-            //cout << " le quedan " << ent[m] << " aristas a " << m << endl;
-
             if(!ent[m]) s.push(m);
         }
     }
-
-    return (aris == 0);
 }
 
 int main(){
     int n, m, x, y;
 
     while(scanf("%d %d", &n, &m), n, m){//cantidad de nodos y aristas
-        vqi grafo(n + 1);
-        memset(vis, 0, sizeof(vis));
-        memset(ent, 0, sizeof(ent));
+        vii grafo(n + 1);
+        ent.assign(n + 1, 0);
 
         for(int i = 0; i < m; i++){
             scanf("%d %d", &x, &y);
-            grafo[x].push(y);
+            grafo[x].push_back(y);
             ent[y]++;
         }
 
-        bool aux = topological_sort(grafo, n, m);
-        //cout << "ggg " << aux << endl;
-        if(res.size() == n)
+        topological_sort(grafo, n);
+        
+        if(res.size() == n)//ordenamiento topologico
             for(int i = 0; i < res.size(); i++) printf("%d\n", res[i]);
         else
-            printf("IMPOSSIBLE\n");//contiene ciclo!!!
+            printf("hay un ciclo!!!\n");//contiene ciclo!!!
     }
 }
