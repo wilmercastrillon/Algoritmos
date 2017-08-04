14e56e2ebb6223ba919418d38aa079b7fb19c400
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-08-03 20:58:12 -0500

    Add files via upload

---
+++ otros/codigo gray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int gray(int n) {
+	return n ^ (n >> 1);
+}
+
+int num(int gray) {
+	int n = 0;
+	for (; gray; gray >>= 1)
+		n ^= gray;
+	return n;
+}
+
+int main() {
+	int n, m;
+
+	while(cin >> n){
+        n = gray(n);
+        m = num(n);
+        cout << "numero gray " << n << endl;
+        cout << "original " << m << endl;
+	}
+}
