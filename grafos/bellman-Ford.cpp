71075b0f2794e4a81841ec6116e7b0b46191f482
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-08-20 21:27:42 -0500

    Update bellman-Ford.cpp

--- grafos/bellman-Ford.cpp
+++ grafos/bellman-Ford.cpp
@@ -2,26 +2,25 @@
 #include <vector>
 
 using namespace std;
-typedef pair<double, pair<int, int>> pdii;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;//(peso, nodo padre, nodo hijo)
 typedef vector<int> vi;
 
-#define mpii(a, b) pair<int, int>(a, b)
-#define mpdii(a, b, c) pdii(a, mpii(b, c))//(peso, nodo padre, nodo hijo)
-#define inf 1000
+#define mpdii(a, b, c) iii(a, ii(b, c))
+#define inf 1000000000
 
-vector<pdii> grafo; //lista de incidencia
-vector<vi> matriz(10);
-int padre[10];
+vector<iii> grafo; //lista de incidencia
+int padre[10];//opcional
 
-bool BellmanFord(vector<pdii> &lista, int nodos, int inicio, vector<int> &dis){
-	
+bool BellmanFord(vector<iii> &lista, int nodos, int inicio, vector<int> &dis){
+
 	for(int i = 0; i < nodos; i++){
 		dis[i] = inf;
 		padre[i] = i;
 	}
 	dis[inicio] = 0;
 	int aux;
-	
+
 	for (int i = 0; i < nodos; i++)
 		for (int j = 0; j < lista.size(); j++)
 		{
@@ -32,16 +31,19 @@ bool BellmanFord(vector<pdii> &lista, int nodos, int inicio, vector<int> &dis){
 				padre[lista[j].second.second] = lista[j].second.first;
 			}
 		}
-		
+
 		for(int j = 0; j < nodos; j++){
 			aux = dis[lista[j].second.first] + lista[j].first;
-			if(dis[lista[j].second.second] > aux) return false;
+			if(dis[lista[j].second.second] > aux)
+                return false;//existe ciclo!!!
 		}
 	return true;
 }
 
+vector<vi> matriz(10);
+
 int main(){
-	
+
 	grafo.push_back(mpdii(2, 0, 1));
 	grafo.push_back(mpdii(2, 1, 0));
 	grafo.push_back(mpdii(4, 1, 2));
@@ -58,12 +60,12 @@ int main(){
 	grafo.push_back(mpdii(10, 8, 3));
 	grafo.push_back(mpdii(1, 9, 4));
 	grafo.push_back(mpdii(9, 9, 7));
-	
+
 	for(int j = 0; j < 10; j++){
 		vector<int> vec(10);
 		matriz[j] = vec;
 	}
-	
+
 	for(int j = 0; j < 10; j++)
 		if(BellmanFord(grafo, 10, j, matriz[j])){
 			for(int i = 0; i < 10; i++) cout << matriz[j][i] << " ";
@@ -71,6 +73,6 @@ int main(){
 		}else{
 			cout << "ciclo negativo!!!" << endl;
 		}
-	
+
 	return 0;
 }
