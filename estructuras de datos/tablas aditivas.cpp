bbb17cdf8257ce86439e9799c1e739c9cf275613
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-08-20 21:18:08 -0500

    Update tablas aditivas.cpp

--- estructuras de datos/tablas aditivas.cpp
+++ estructuras de datos/tablas aditivas.cpp
@@ -13,7 +13,7 @@ int tab[10][10], tab2[10][10];
 9 5 3 7 8
 */
 
-void pre(){
+void build(){
     memset(tab2, 0, sizeof(tab2));
     tab2[1][1] = tab[0][0];
     for (int i = 2; i < 6; i++) tab2[i][1] = tab2[i-1][1] + tab[i - 1][0];
@@ -22,15 +22,16 @@ void pre(){
     for (int i = 2; i < 6; i++)
 		for (int j = 2; j < 6; j++)
             tab2[i][j] = tab2[i][j - 1] + tab2[i - 1][j] + tab[i - 1][j - 1] - tab2[i - 1][j - 1];
+    //ejemplo: acumulado de 2,2 hasta 5,5
+    //tab2[5][5] - tab2[2][5] - tab2[5][2] + tab2[2][2]
 }
 
 int main(){
-
     for (int i = 0; i < 5; i++)
 		for (int j = 0; j < 5; j++)
 			cin >> tab[i][j];
+    build();
 
-    pre();
     cout << "tabla aditiva:" << endl;
     for (int i = 0; i < 6; i++){
 		for (int j = 0; j < 6; j++)
