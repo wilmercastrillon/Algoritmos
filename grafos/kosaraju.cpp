894461cd830d393281219522d0d6a01f0111c806
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-08-28 19:48:05 -0500

    kruskal y kosaraju

--- grafos/kosaraju.cpp
+++ grafos/kosaraju.cpp
@@ -10,28 +10,22 @@ vector<vi> grafo(5), transpuesto(5), comp;
 stack<int> pila;
 bool vis[5];
 
-void dfs(int n){
+void dfs(int n, vector<vi> lista, bool f, vi &grupo){
 	vis[n] = true;
-	
-	for (int i = 0; i < grafo[n].size(); i++)
-		if (!vis[grafo[n][i]]) dfs(grafo[n][i]);
-	
-	pila.push(n);
-}
+	if(!f) grupo.push_back(n);
 
-void dfs_2(int n, vi &grupo){
-	vis[n] = true;
-	grupo.push_back(n);
-	
-	for (int i = 0; i < transpuesto[n].size(); i++)
-		if (!vis[transpuesto[n][i]]) dfs_2(transpuesto[n][i], grupo);
+	for (int i = 0; i < lista[n].size(); i++)
+		if (!vis[lista[n][i]]) dfs(lista[n][i], lista, f, grupo);
+
+    if(f) pila.push(n);
 }
 
 void kosaraju(){
 	memset(vis, false, sizeof(vis));
+	vi no_se_utiliza;
 	for (int i = 0; i < 5; i++)
-		if(!vis[i]) dfs(i);
-	
+		if(!vis[i]) dfs(i, grafo, true, no_se_utiliza);
+
 	memset(vis, false, sizeof(vis));
 	int n;
 	while(pila.size())
@@ -41,32 +35,32 @@ void kosaraju(){
 		if (!vis[n])
 		{
 			vi vec;
-			dfs_2(n, vec);
+			dfs(n, transpuesto, false, vec);
 			comp.push_back(vec);
 		}
+	}
+
+	for (int i = 0; i < comp.size(); i++){
+		for (int j = 0; j < comp[i].size(); j++) cout << comp[i][j] << " ";
+		cout << endl;
 	}
 }
 
 int main() {
-	
+
 	grafo[0].push_back(1);
 	grafo[1].push_back(2);
 	grafo[2].push_back(4);
 	grafo[1].push_back(3);
 	grafo[3].push_back(0);
-	
+
 	transpuesto[1].push_back(0);
 	transpuesto[2].push_back(1);
 	transpuesto[4].push_back(2);
 	transpuesto[3].push_back(1);
 	transpuesto[0].push_back(3);
-	
+
 	kosaraju();
-	
-	for (int i = 0; i < comp.size(); i++){
-		for (int j = 0; j < comp[i].size(); j++) cout << comp[i][j] << " ";
-		cout << endl;
-	}
-	
+
 	return 0;
 }
--- grafos/kruskal.cpp
+++ grafos/kruskal.cpp
@@ -4,15 +4,11 @@
 
 using namespace std;
 
-typedef pair<int, pair<int, int>> piii;
-#define mpii(a, b) pair<int, int>(a, b)
-#define mpiii(a, b, c) piii(a, mpii(b, c))
-
 struct union_find{
 	int padre[100];
 	int rango[100];
 	int tam = 0;
-	
+
 	void iniciar(int n){
 		for (int i = 0; i < n; i++)
 		{
@@ -21,38 +17,44 @@ struct union_find{
 		}
 		tam = n;
 	}
-	
+
 	int raiz(int x){
 		if(padre[x] == x) return x;
 		return raiz(padre[x]);
 	}
-	
+
 	void unir(int x, int y){
 		x = raiz(x);
 		y = raiz(y);
-		
+
 		if(rango[x] > rango[y]){
 			padre[y] = x;
 			return;
 		}
-		
+
 		padre[x] = y;
 		if(rango[y] == rango[x]) rango[y]++;
 	}
-	
+
 	bool MismoGrupo(int x, int y){
 		return raiz(x) == raiz(y);
 	}
 };
 
-vector<piii> grafo;
+typedef pair<int, int> ii;
+typedef pair<int, ii> piii;//peso, origen y destino
+#define mpiii(a, b, c) piii(a, ii(b, c))
+//ejemplo de insertar:
+//grafo.push_back(mpiii(7, 0, 1))
+
+vector<piii> grafo;//lista de incidencia
 union_find arbol;
 
 int kruskal(vector<piii> lista, int nodos, union_find &uf){
 	sort(lista.begin(), lista.end());
 	uf.iniciar(nodos);
 	int acum = 0, ejes = 0, n = nodos - 1;
-	
+
 	for (int i = 0; i < lista.size(); i++)
 	{
 		if (!uf.MismoGrupo(lista[i].second.first, lista[i].second.second))
@@ -62,14 +64,13 @@ int kruskal(vector<piii> lista, int nodos, union_find &uf){
 			acum += lista[i].first;
 			if(ejes == n) return acum;
 		}
-		
+
 	}
-	
 	return -1;
 }
 
 int main(){
-	
+
 	grafo.push_back(mpiii(7, 0, 1));
 	grafo.push_back(mpiii(5, 0, 3));
 	grafo.push_back(mpiii(8, 1, 2));
@@ -81,15 +82,15 @@ int main(){
 	grafo.push_back(mpiii(8, 4, 5));
 	grafo.push_back(mpiii(9, 4, 6));
 	grafo.push_back(mpiii(11, 5, 6));
-	
+
 	cout << "peso " << kruskal(grafo, 7, arbol) << endl;
-	
+
 	for(int i = 0; i < 7; i++) cout << i << " ";
 	cout << endl;
 	for(int i = 0; i < 7; i++) cout << arbol.padre[i] << " ";
 	cout << endl;
 	for(int i = 0; i < 7; i++) cout << arbol.rango[i] << " ";
 	cout << endl;
-	
+
 	return 0;
 }
